5e4e8d91219a3f587c7eb6a88e2461d3b260f8fb
carsonbcobb <carsonbcobb@gmail.com>
2020-06-07 11:34:03 -0700

    typo fixes

--- src/pages/About-Me/About-Me.component.jsx
+++ src/pages/About-Me/About-Me.component.jsx
@@ -25,7 +25,7 @@ const AboutMePage = () => (
 		<BodyAbout>
 			<Body>
 				My name is Carson Cobb. I am from Fresno, CA and currently working as a
-				Front-End Web Developer/Open Souce Contributor. I have experience with
+				Front-End Web Developer/Open Source Contributor. I have experience with
 				HTML, CSS, SCSS, Bootstrap, Javascript, and React. Constantly learning
 				new coding languages and expanding my horizons, I am self-taught but
 				have also taken courses from Zero to Mastery, Academind, and others.
